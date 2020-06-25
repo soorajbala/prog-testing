31b30b6620fcb9c3ef8313fdc3b8dfacc572f85a
SoorajBala <sooraj.bala@gmail.com>
2020-06-25 22:44:50 +0530

    Updates

---
+++ precache-manifest.f05df2e0af7756a03dd30940a4e1af43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1687a2ec2d19e483d3555c29252145c6",
+    "url": "/soorajbala/prog-testing/index.html"
+  },
+  {
+    "revision": "fa1c6da91afe92ad5130",
+    "url": "/soorajbala/prog-testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7a4b6db1e6a10b2ada0",
+    "url": "/soorajbala/prog-testing/static/js/2.01f22b3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/soorajbala/prog-testing/static/js/2.01f22b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1c6da91afe92ad5130",
+    "url": "/soorajbala/prog-testing/static/js/main.0dead3da.chunk.js"
+  },
+  {
+    "revision": "4507694e35a07340cd50",
+    "url": "/soorajbala/prog-testing/static/js/runtime-main.17144ca0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/soorajbala/prog-testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
